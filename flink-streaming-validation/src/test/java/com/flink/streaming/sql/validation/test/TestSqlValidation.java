478ac2e8d09b215911eeed3f4dc356ebf1c449ac
jiachuan.zhu <jiachuan.zhu@kyligence.io>
2022-01-21 23:29:41 +0800

    Automatically runs unit test on pull requests

--- flink-streaming-validation/src/test/java/com/flink/streaming/sql/validation/test/TestSqlValidation.java
+++ flink-streaming-validation/src/test/java/com/flink/streaming/sql/validation/test/TestSqlValidation.java
@@ -20,25 +20,19 @@ public class TestSqlValidation {
 
         List<String> list= Lists.newArrayList();
         list.add("CREATE view print_table AS\n" +
-                " select count(f0) AS c from source_table");
+                " select count(f0) AS c from source_table;");
         list.add("CREATE TABLE print_table2 (\n" +
                 " c BIGINT\n" +
                 ") WITH (\n" +
                 " 'connector' = 'print'\n" +
-                ")");
+                ");");
         list.add(" CREATE TABLE source_table (\n" +
                 " f0 INT\n" +
                 ") WITH (\n" +
                 " 'connector' = 'datagen',\n" +
                 " 'rows-per-second'='5'\n" +
-                ")");
-        list.add("insert into  print_table2\n" +
-                "\tselect c from print_table");
-
-        //校验失败
-        list.add("insert into  print_table2\n" +
-                "\tselect c froms print_table");
+                ");");
+        list.add("INSERT INTO print_table2 select f0 FROM print_table;");
         SqlValidation.preCheckSql(list);
-
     }
 }
